69e8781e897dd9c3c9590f6b2bc53624797f3126
이준용 <iiiuooyoo@gmail.com>
2023-09-15 15:04:01 +0900

    undo express
    /api/index.js 경로에 express 프로젝트를 생성해서 구성하고, vercel에 올리면 알아서 해줌

--- api/index.js
+++ api/index.js
@@ -2,27 +2,27 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 
-// const app = express();
-// const server = createServer(app);
-// const port = 5000;
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*",
-//         methods: ["GET", "POST"],
-//     },
-// });
+const app = express();
+const server = createServer(app);
+const port = 5000;
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
-// app.get("/", (request, response) =>
-//     response.json({ path: "/", data: "Hello World!" }),
-// );
+app.get("/", (request, response) =>
+    response.json({ path: "/", data: "Hello World!" }),
+);
 
-// io.on("connection", (socket) => {
-//     const { id } = socket;
-//     console.log(`a user connected ${id}`);
+io.on("connection", (socket) => {
+    const { id } = socket;
+    console.log(`a user connected ${id}`);
 
-//     socket.on("disconnect", () => {
-//         console.log(`user disconnected ${id}`);
-//     });
-// });
+    socket.on("disconnect", () => {
+        console.log(`user disconnected ${id}`);
+    });
+});
 
-// server.listen(port, () => console.log(`server running at port: ${port}`));
+server.listen(port, () => console.log(`server running at port: ${port}`));
