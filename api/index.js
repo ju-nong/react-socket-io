9d84201cb2906e74f8937e6ef4bf41d0a96836ea
이준용 <iiiuooyoo@gmail.com>
2023-09-15 16:05:17 +0900

    feat : 채팅 기능 추가

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import { Server } from "socket.io";
 const app = express();
 const server = createServer(app);
 const port = 5000;
+
 const io = new Server(server, {
     cors: {
         origin: "*",
@@ -12,17 +13,36 @@ const io = new Server(server, {
     },
 });
 
-app.get("/", (request, response) =>
-    response.json({ path: "/", data: "Hello World!" }),
-);
+const chat = [];
+
+// app.get("/", (request, response) =>
+//     response.json({ path: "/", data: "Hello World!" }),
+// );
 
 io.on("connection", (socket) => {
-    const { id } = socket;
+    const { id } = socket; // Socket에 고유 ID
+
+    // [Server] Socket 연결 확인
     console.log(`a user connected ${id}`);
 
+    // [Events] 연결 끊기 이벤트 등록
     socket.on("disconnect", () => {
+        // [Server] Socket 연결 끊김 확인
         console.log(`user disconnected ${id}`);
     });
+
+    // [Events] 메시지 전송 이벤트 등록
+    socket.on("REQUEST MESSAGE", (message) => {
+        // 채팅 업데이트
+        chat.push({ id, message });
+
+        // 모든 Socket한테 업데이트 된 채팅 전송
+        socket.broadcast.emit("RESPONSE MESSAGE", chat);
+        socket.emit("RESPONSE MESSAGE", chat);
+    });
+
+    // [Call] 연결된 Sokect한테 전체 채팅 전송
+    socket.emit("RESPONSE MESSAGE", chat);
 });
 
 server.listen(port, () => console.log(`server running at port: ${port}`));
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useEffect, KeyboardEvent, useState } from "react";
+import styled from "@emotion/styled";
+import { socket } from "./socket";
+
+const InputStyled = styled.input`
+    border: 1px solid #000;
+    padding: 4px 12px;
+    border-radius: 4px;
+`;
+
+type Chat = {
+    id: string;
+    message: string;
+};
 
 function App() {
-    useEffect(() => {
-        const socket = io("http://localhost:5000");
+    const [chat, setChat] = useState<Chat[]>([]);
 
-        console.log(socket);
+    useEffect(() => {
+        socket.on("RESPONSE MESSAGE", (response: Chat[]) => {
+            setChat(response);
+        });
 
         return () => {
             socket.disconnect();
         };
-    });
+    }, []);
+
+    function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
+        const { key, target } = event;
+
+        if (key === "Enter") {
+            socket.emit("REQUEST MESSAGE", (target as HTMLInputElement).value);
+
+            (target as HTMLInputElement).value = "";
+        }
+    }
 
     return (
         <>
             <div>Hello World</div>
+            <br />
+            <ul>
+                {chat.map(({ id, message }, index) => (
+                    <li key={index}>
+                        {id}: {message}
+                    </li>
+                ))}
+            </ul>
+            <InputStyled type="text" onKeyDown={handleKeyDown} />
         </>
     );
 }
